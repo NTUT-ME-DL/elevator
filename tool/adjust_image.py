47e52ba91a7552689b1ca037a6d1cca0ea317a8d
DarkFanXing <36408071+darkfanxing@users.noreply.github.com>
2020-05-26 15:33:32 +0800

    Add tool that adjust value and saturation of image

---
+++ tool/adjust_image.py
@@ -0,0 +1,33 @@
+import os
+import cv2
+import numpy as np
+
+data_types = ['training', 'test']
+for data_type in data_types:
+  for floor in range(11):
+      root_path = "C:/Users/AILab/Documents/elevator/data/0520/"
+
+      folder_path = "{}{}/{}f/".format(root_path, data_type, floor)
+      file_names = os.listdir(folder_path)
+
+      for file_name in file_names:
+          file_path = folder_path + file_name
+
+          orig_image = cv2.imread(file_path)
+          image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2HSV)
+
+          image[..., 1] -= 20
+          image[image[..., 1] >= 235] = 0
+
+          image[..., 2] -= 110
+          image[image[..., 2] >= 145] = 0
+
+          image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+          image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+          cv2.imwrite("{}adjust_{}/{}f/{}".format(root_path, data_type, floor, file_name), image)
+          # cv2.imshow("123", np.vstack((image, orig_image)))
+
+          # key = cv2.waitKey(0)
+          # if key == 27:
+          #   cv2.destoryAllWindows()
+          #   break
